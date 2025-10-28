37dbba57c7295e48c48e5fd11c7d9ba79db06cfd
Kuruguntla Asritha <asritha.kuruguntla@gmail.com>
2025-10-28 12:04:56 +0530

    Delete LibraryManagementSystem directory

--- LibraryManagementSystem/src/Exceptions/BookNotBorrowedException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class BookNotBorrowedException extends Exception{
-public BookNotBorrowedException(String msg) {
-	super(msg);
-}
-}
--- LibraryManagementSystem/src/Exceptions/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class BookNotFoundException extends Exception{
-  public BookNotFoundException(String msg) {
-	  super(msg);
-  }
-}
