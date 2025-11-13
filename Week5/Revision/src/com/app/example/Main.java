bfdcbcff1ccd85b4a88d2b3d5f88d6697366fec6
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-11-13 17:12:21 +0530

    m

--- Week5/Revision/src/com/app/example/Main.java
+++ Week5/Revision/src/com/app/example/Main.java
@@ -1,10 +1,15 @@
 package com.app.example;
+import com.app.example.Author;
+import com.app.example.Book;
+import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Calendar;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -16,6 +21,15 @@ public class Main {
 		
 		Book book = new Book(1,"Geetanjali",listOfAuthors,Genre.FICTION,Edition.BASIC,200.00f,publishedTime);
 		System.out.println(book);
+		
+		final Logger logger = LogManager.getLogger(Main.class);//we use LogManager to get the logger instance
+		//we give Main.class to associate the logger with this class
+		//getLogger is a static method that returns a logger instance
+		
+		logger.info("Application started");
+		logger.debug("Book created: " + book);
+		logger.info("Application ended");
+		
 
 	}
 
