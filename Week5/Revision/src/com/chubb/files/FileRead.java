6c9026101fa8bd8cc88a23ce7df74965c6f87f4b
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-11-13 15:40:22 +0530

    modified FileRead

--- Week5/Revision/src/com/chubb/files/FileRead.java
+++ Week5/Revision/src/com/chubb/files/FileRead.java
@@ -2,22 +2,36 @@ package com.chubb.files;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.*;
 
 public class FileRead {
 public static void main(String[] args) {
 	try {
-	BufferedReader br = new BufferedReader(new FileReader("sample.txt"));
+	BufferedReader bufferedReader = new BufferedReader(new FileReader("sample.txt"));
 	String line;
 	int count=0;
-	while((line = br.readLine()) != null) {
-		String [] words = line.split(" ");
-		for(String word: words) {
-			if(word.equalsIgnoreCase("India"))
-			count++;
-		}
-	}
-	System.out.println("The word 'India' occurred " + count + " times in the file.");
-	br.close();
+//	while((line = br.readLine()) != null) {
+//		String [] words = line.split(" ");
+//		for(String word: words) {
+//			if(word.equalsIgnoreCase("India"))
+//			count++;
+//		}
+//	}
+
+//	while((line = br.readLine()) != null) {
+//		String [] words = line.split(" ");
+//		count +=(int) Arrays.stream(words)
+//				.filter(word -> word.equalsIgnoreCase("India"))
+//				.count();
+//	}
+	long c = bufferedReader.lines().flatMap(l-> Arrays.stream(l.split(" "))).filter(word->word.equalsIgnoreCase("India")).count();
+	
+	
+	//System.out.println("The word 'India' occurred " + count + " times in the file.");
+	System.out.println("The word 'India' occurred " + c + " times in the file.");
+
+	bufferedReader.close();
 	} catch (Exception e) {
 		e.printStackTrace();
 	
