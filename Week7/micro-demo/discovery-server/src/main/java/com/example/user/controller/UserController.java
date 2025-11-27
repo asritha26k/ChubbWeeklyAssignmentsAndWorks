db15d1d0d47a36d36d5e2a5e9795de42b2016a9a
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-11-27 17:21:56 +0530

    added week 7

---
+++ Week7/micro-demo/discovery-server/src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+@RestController
+public class UserController {
+    @GetMapping("/users")
+    public List<String> users() {
+        return List.of("Alice","Bob","Charlie");
+    }
+}
